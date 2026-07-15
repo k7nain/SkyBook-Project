54f1fb1db825dd08fb13953862c4f54a8bd9e61a
k7nain <murslizadkenan@gmail.com>
2026-07-15 15:06:32 +0400

    feat: Interfaces created

---
+++ FlyzenApi/src/Core/FlyzenApi.Application/Common/Models/Result.cs
@@ -0,0 +1,19 @@
+namespace FlyzenApi.Application.Common.Models
+{
+    public class Result<T>
+    {
+        public bool Success { get; set; }
+        public T? Data { get; set; }
+        public string? Message { get; set; }
+
+        public static Result<T> SuccessResult(T data, string? message = null)
+        {
+            return new Result<T> { Success = true, Data = data, Message = message };
+        }
+
+        public static Result<T> FailureResult(string message)
+        {
+            return new Result<T> { Success = false, Message = message };
+        }
+    }
+}
---
+++ FlyzenApi/src/Core/FlyzenApi.Application/Interfaces/IEmailService.cs
@@ -0,0 +1,9 @@
+using System.Threading.Tasks;
+
+namespace FlyzenApi.Application.Interfaces
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(string to, string subject, string body, byte[]? attachment = null, string? attachmentName = null);
+    }
+}
